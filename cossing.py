75e3d6e5781dde001a24ccf9c89be2b152ba3af6
FisherChen <326112910@qq.com>
2017-05-28 10:32:54 +0800

    Update cossing.py
    提交作业：验证哥德巴赫猜想

--- cossing.py
+++ cossing.py
@@ -78,6 +78,37 @@ def week3_generateYH(m):
         Layer = [week3_generateYH_help(Layer,x,i) for i in range(x+1)]
     print(Layer)
 
+def week4_Goldbach( num ):
+    # 验证哥德巴赫猜想
+    num = int(num)
+    if mod(num,2)==0:
+        for m in range(2,num):
+            n = num - m
+            if week4_isPrime(m) and week4_isPrime(n):
+                print(m,' + ',n,' == ',num)
+                return
+    elif num>5:
+        for m in range(2,num-4):
+            for n in range(2,num-2-m):
+                k = num-m-n
+                if week4_isPrime(m) and week4_isPrime(n) and week4_isPrime(k):
+                    print(m,' + ',n,' + ',k,' == ',num)
+                    return
+    print('Failed')
+    return
+
+def week4_isPrime( num ):
+    # 判断是否质数
+    num = int( num )
+    if num>=1 and num<=3:
+        return True
+    if mod(num,2)==0:
+        return False
+    for x in range(2, int( sqrt(num) )+1 ):
+        if mod(num,x)==0:
+            return False
+    return True
+
 if __name__ == '__main__':
     # week1_romantic()
     # week2_magic(8)
